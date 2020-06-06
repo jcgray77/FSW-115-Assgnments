657be633c867316fa7fce12c4edada0a280e2194
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-06-06 19:38:26 -0400

    Week1

---
+++ JSON Deli.js
@@ -0,0 +1,61 @@
+let menu = {
+"appetizer": [
+{
+"name": "Jalapeno Poppers",
+"ingredients": ["Jalapenos", "Cream Cheese", "Salsa"],
+"description": "Delicous ans Spicey"
+},
+{
+"name": "Nachos", 
+"ingredients": ["Chips", "Mexican Cheese", "Ground Beef"],
+"descrition:": "Ariba"
+}
+],
+"cold sandwhiches": [
+{
+"name": "Ham and Cheese",
+"ingredients": ["Sourdough bread", "American Cheese", "Honey Baked Ham"],
+"description": "All-American Goodness"
+},
+{
+"name": "Bologna",
+"ingredients": ["Bologna", "White bread", "Horsereadish mustard"],
+"description": "Perfect for a picnic"
+}
+],
+"Hot Sandwhiches": [
+{
+"name": "Meatball",
+"ingredients": ["Meatballs", "Mozzerella", "Marinara"],
+"descriptiion": "Mama Mia Manja"
+},
+{
+"name": "Philly Cheesesteak",
+"ingredients": ["Steak", "Provolone", "Hoagie"],
+"description": "Motown Philly Back Again"
+}
+],
+"Drinks": [
+{
+"name": "Mojito",
+"ingredients": ["Rum", "Soda Water", "Mint"],
+"description": "So refreshing"
+},
+{
+"name": "Shirley Temple",
+"ingredients": ["Ginger Ale", "Grenadine", "Maraschino Cherrie"],
+"description": "Fun for ALL ages"
+}
+],
+"dessert": [
+{
+"name": "Strawberry Cheesecake",
+"ingredients": ["Graham cracker crust", "Cheese", "Strawberries"],
+"description": "Perfection"
+},
+{
+"name": "Brownie Sundae",
+"ingredients": ["Brownie", "Vanilla Ice Cream", "Whip Cream"],
+"description": "Decadence"
+}
+]
